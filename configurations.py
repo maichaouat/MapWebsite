f9dda00ae321c60fecc4882324a9f302e1b3f448
nadav ilany <nadavilany1@gmail.com>
2024-05-04 19:57:06 +0300

    part 4 automated query api according to bbox

---
+++ configurations.py
@@ -0,0 +1,8 @@
+
+
+# Define the access token and bounding box
+access_token = "MLY|25204399145874923|91edfe348b19b66200b3119e41b28487"
+bbox = "34.768,32.068,34.770,32.070"  # Provided bounding box
+
+# Define the API URL
+api_url = f"https://graph.mapillary.com/images?access_token={access_token}&fields=id&bbox={bbox}"
